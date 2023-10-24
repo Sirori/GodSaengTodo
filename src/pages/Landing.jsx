5443627d6aa34d8bf2ae7fde2ced52d0423f967b
itzwe <itzwewewe@naver.com>
2023-10-24 16:06:06 +0900

    [add] 1차 작업물

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,21 +1,21 @@
 
 import { Helmet } from 'react-helmet-async';
-import { Link } from 'react-router-dom';
-
-
+import LandingButton from '@/component/button/LandingButton';
+// import { ReactComponent as Img } from "@/assets/img.svg";
+import Img from '@/assets/img.svg'
 function Landing() {
   return (
     <>
       <Helmet>
         <title>GodSaengTodo -Landing</title>
       </Helmet>
-      
-      <div className="">
-        안녕하세요
-        <Link >
-        
-        </Link>
-      </div>
+        <div className="w-full flex flex-col text-center gap-12 pt-44">
+          <span className='block'>시연님 글씨 스타일 받아오기</span>
+          <img src={Img} alt="Landing Logo"/>
+          {/* <Img/>  안됨  */}
+          <LandingButton/>
+        </div>
+  
     </>
   );
 }
