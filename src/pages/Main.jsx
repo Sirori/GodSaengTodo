9a2884d639e93ec8bec9cfd80a56f4e6d2e1fe10
itzwe <itzwewewe@naver.com>
2023-10-31 15:20:56 +0900

    [modify] main 구조 수정

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,17 +1,39 @@
-import ImgComponent from '@/component/layout/ImgComponent';
+import WeatherButton from '@/component/button/WeatherButton';
+import Spinner from '@/component/layout/Spinner';
 import { Helmet } from 'react-helmet-async';
+import useBackgroundImage from '@/hooks/useBackgroundImage';
+import Calender from '@/component/Calender';
+
 
 
 function Main() {
   
-  
+  const {bg, isLoading} = useBackgroundImage();
+
+  if(isLoading){
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <Spinner size={200} />
+      </div>
+    );
+  }
   return (
     <>
       <Helmet>
         <title>GodSaengTodo -Main</title>
       </Helmet>
       
-      <ImgComponent/>
+      
+      <div
+          style={{
+            backgroundImage: `url(${bg})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            height: "100vh",
+          }}
+          >
+          <WeatherButton/>
+          </div>
     </>
   );
 }
